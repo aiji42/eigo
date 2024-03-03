f4cd6fb883948506db2d048d9619449309306065
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2024-03-03 18:11:22 +0900

    improve translate service

--- src/service-bindings/translate.ts
+++ src/service-bindings/translate.ts
@@ -22,12 +22,15 @@ export default {
 		if (!payload || typeof payload !== 'object' || !('text' in payload) || typeof payload.text !== 'string')
 			return new Response('400 Bad Request', { status: 400 });
 
-		const token = await getGoogleToken(JSON.parse(env.GOOGLE_AUTH));
 		const cacheKey = `translate:${await sha256(payload.text)}`;
 		const value = await env.CACHE.get(cacheKey);
 
-		if (value) return new Response(value);
+		if (value) {
+			console.log('cache hit');
+			return new Response(value);
+		}
 
+		const token = await getGoogleToken(JSON.parse(env.GOOGLE_AUTH));
 		const translated = await translateByGoogle(token, payload.text);
 
 		context.waitUntil(env.CACHE.put(cacheKey, translated));
