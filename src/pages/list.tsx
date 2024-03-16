0b93e0d3a1af86da78a7cb86f4b166de8ae0bc71
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2024-03-16 20:57:24 +0900

    improve list page

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -30,29 +30,31 @@ const Page = () => {
 	return (
 		<>
 			<StickyHeader />
-			<ul className="p-2">
-				{data
-					?.flatMap((data) => data)
-					.map((item) => (
-						<li key={item.id}>
-							<Link to={`/${item.id}`}>
-								<div className="mb-4 flex items-center justify-between gap-4 rounded-md hover:bg-neutral-800">
-									<img src={item.thumbnailUrl ?? ''} alt={item.title} className="size-32 rounded-md object-cover" />
-									<div className="min-w-0 flex-1">
-										<h2 className="overflow-hidden overflow-ellipsis text-xl md:text-3xl">{item.title}</h2>
-										<p className="text-gray-400">{displayRelativeTime(item.publishedAt)} ago</p>
+			<div className="flex flex-col gap-4">
+				<ul className="p-2">
+					{data
+						?.flatMap((data) => data)
+						.map((item) => (
+							<li key={item.id}>
+								<Link to={`/${item.id}`}>
+									<div className="mb-4 flex items-center justify-between gap-4 rounded-md hover:bg-neutral-800">
+										<img src={item.thumbnailUrl ?? ''} alt={item.title} className="size-32 rounded-md object-cover" />
+										<div className="min-w-0 flex-1">
+											<h2 className="overflow-hidden overflow-ellipsis text-xl md:text-3xl">{item.title}</h2>
+											<p className="text-gray-400">{displayRelativeTime(item.publishedAt)} ago</p>
+										</div>
 									</div>
-								</div>
-							</Link>
-						</li>
-					))}
-			</ul>
-			{!isValidating && <div ref={ref} aria-hidden="true" />}
-			{isValidating && (
-				<div className="fixed inset-0 flex items-center justify-center">
-					<LoadingIcon />
-				</div>
-			)}
+								</Link>
+							</li>
+						))}
+				</ul>
+				{!isValidating && <div ref={ref} aria-hidden="true" />}
+				{isValidating && (
+					<div className="relative mb-12">
+						<LoadingIcon size={32} />
+					</div>
+				)}
+			</div>
 		</>
 	);
 };
